9b7f317264138a8e66955c9e7c0e5f915f3c4c01
MweziMDogo <fijifolf@gmail.com>
2025-02-10 18:14:38 -0500

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/cd-demo",
+  //create a base key and add the github pages URL here
+  //Ex. base: "/<gitHubRepoName>",
+});
